d050fa2ab47f165b8ed43e520799b5ed1051f7c8
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-06-16 11:58:24 +0000

    updated Database to postgresql

--- interiorhealth-backend/config/settings.py
+++ interiorhealth-backend/config/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',  # Will later change to "db" in Docker
+        'PORT': '5432',
     }
 }
 
