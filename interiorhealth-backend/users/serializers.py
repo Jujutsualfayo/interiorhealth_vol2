197b30bc8c0a3e437778aea4f56b2c1c6b7759d2
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-06-17 08:31:15 +0000

    created urls for the views to feed on

---
+++ interiorhealth-backend/users/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth.password_validation import validate_password
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password', 'role']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            role=validated_data.get('role', 'patient')
+        )
+        return user
--- interiorhealth-backend/users/views.py
+++ interiorhealth-backend/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+class RegisterUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+
