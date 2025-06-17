b1cc2bf45df7982d9f9f26b3753359360ab83d86
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-06-17 07:33:25 +0000

    dockerised entire backend

--- interiorhealth-backend/config/settings.py
+++ interiorhealth-backend/config/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'NAME': config('POSTGRES_DB'),
         'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
-        'HOST': config('POSTGRES_HOST', default='localhost'),
+        'HOST': config('POSTGRES_HOST', default='db'),
         'PORT': config('POSTGRES_PORT', default='5432'),
 
     }
