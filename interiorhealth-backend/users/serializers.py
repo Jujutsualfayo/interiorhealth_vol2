2a5a3f8064789ba36d8b39a3482e5a8b9a6129d9
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-06-17 10:36:12 +0000

    update

--- interiorhealth-backend/users/serializers.py
+++ interiorhealth-backend/users/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 from .models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    
+
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password', 'role']
@@ -12,7 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            username=validated_data['username'],
+            username=validated_data.get('username', validated_data['email']),
             email=validated_data.get('email', ''),
             password=validated_data['password'],
             first_name=validated_data.get('first_name', ''),
@@ -20,3 +21,16 @@ class UserSerializer(serializers.ModelSerializer):
             role=validated_data.get('role', 'patient')
         )
         return user
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        token['role'] = user.role
+        return token
+
+    def validate(self, attrs):
+        # Use email as username
+        attrs['username'] = attrs.get('email')
+        return super().validate(attrs)
--- interiorhealth-backend/users/urls.py
+++ interiorhealth-backend/users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from users.views.register import RegisterUserView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from users.views.register import RegisterHealthWorkerView
+from users.views.register import RegisterUserView, RegisterHealthWorkerView
+from users.views.token import CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  # FIXED
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("register/healthworker/", RegisterHealthWorkerView.as_view(), name="register_health_worker"),
 ]
