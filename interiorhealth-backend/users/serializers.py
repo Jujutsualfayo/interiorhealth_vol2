462ecfb5ee13a758ee806b189deae673decd6d26
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-06-17 09:55:01 +0000

    updated read only for admin

--- interiorhealth-backend/users/serializers.py
+++ interiorhealth-backend/users/serializers.py
@@ -8,6 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password', 'role']
+        read_only_fields = ('role',)
 
     def create(self, validated_data):
         user = User.objects.create_user(
