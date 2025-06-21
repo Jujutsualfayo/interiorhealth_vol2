c1c75099af08a8de232c96cc334f293447ac3812
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-06-21 13:16:01 +0000

    added rbac for patients

--- interiorhealth-backend/users/permissions.py
+++ interiorhealth-backend/users/permissions.py
@@ -13,3 +13,10 @@ class IsHealthWorker(permissions.BasePermission):
     """
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == 'health_worker'
+    
+class IsPatient(permissions.BasePermission):
+    """
+    Allows access only to patients.
+    """
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'patient'
