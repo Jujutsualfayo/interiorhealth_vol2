2872e83a787a29ddb8bb7ca181b8e3cb34c16fc1
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-07-04 14:58:02 +0000

    update

--- interiorhealth-backend/drugs/urls/healthworker.py
+++ interiorhealth-backend/drugs/urls/healthworker.py
@@ -1,6 +1,8 @@
 # drugs/urls/healthworker.py
 from django.urls import path
-from drugs.views.healthworker.manage_drugs import DrugCreateView, DrugUpdateDeleteView
+from drugs.views.health_worker.manage_drugs import DrugCreateView, DrugUpdateDeleteView
+
+
 
 urlpatterns = [
     path('create/', DrugCreateView.as_view(), name='create-drug'),
