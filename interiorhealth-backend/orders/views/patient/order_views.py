53f65207879bce2d739c520521926d6269dc17d9
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-07-10 09:55:53 +0000

    update

---
+++ interiorhealth-backend/orders/views/patient/order_views.py
@@ -0,0 +1,19 @@
+# orders/views/patient/order_views.py
+from rest_framework import generics, permissions
+from .serializers import OrderSerializer
+from orders.models import Order
+
+class OrderCreateView(generics.CreateAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(patient=self.request.user)
+
+class OrderListView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(patient=self.request.user).order_by('-ordered_at')
+
