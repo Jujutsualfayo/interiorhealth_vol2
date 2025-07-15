6843b0b99a580920126b3a485b825f7b5504c380
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-07-15 07:53:59 +0000

    updated login view

--- interiorhealth-backend/users/views/token.py
+++ interiorhealth-backend/users/views/token.py
@@ -1,5 +1,7 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny  # ✅ ADD THIS
 from users.serializers import CustomTokenObtainPairSerializer
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+    permission_classes = [AllowAny]  # ✅ ADD THIS
