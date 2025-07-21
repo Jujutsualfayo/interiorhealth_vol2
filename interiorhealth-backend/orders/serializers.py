189d07cd94e452f1c176313ade2a15a1ae9d169f
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-07-21 14:11:10 +0000

    updated

--- interiorhealth-backend/orders/serializers.py
+++ interiorhealth-backend/orders/serializers.py
@@ -13,12 +13,12 @@ class OrderItemSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
     patient_email = serializers.ReadOnlyField(source='patient.email')
-    status = serializers.CharField(read_only=True)
 
     class Meta:
         model = Order
-        fields = ['id', 'patient_email', 'status', 'ordered_at', 'items']
-        read_only_fields = ['status', 'ordered_at']
+        fields = ['id', 'patient_email', 'status', 'total_amount', 'created_at', 'items']
+        read_only_fields = ['patient_email', 'items', 'total_amount', 'created_at']
+
 
     def create(self, validated_data):
         items_data = validated_data.pop('items')
