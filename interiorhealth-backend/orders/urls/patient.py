142e3b0ce750ec7fbdf861ae2c6a00b57ae34a1d
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-08-05 11:36:36 +0000

    payments setup

--- interiorhealth-backend/orders/urls/patient.py
+++ interiorhealth-backend/orders/urls/patient.py
@@ -1,9 +1,12 @@
 # orders/urls/patient.py
 from django.urls import path
 from orders.views.patient.order_views import OrderCreateView, OrderListView
+from views import initiate_payment
 
 urlpatterns = [
     path('create/', OrderCreateView.as_view(), name='order-create'),
     path('my-orders/', OrderListView.as_view(), name='order-list'),
+    path('initiate-payment/', initiate_payment, name='initiate_payment'),
+
 ]
 
--- interiorhealth-backend/orders/views/patient/order_views.py
+++ interiorhealth-backend/orders/views/patient/order_views.py
@@ -1,7 +1,42 @@
 # orders/views/patient/order_views.py
+import requests
 from rest_framework import generics, permissions
 from orders.serializers import OrderSerializer
 from orders.models import Order
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.conf import settings
+
+@api_view(['POST'])
+def initiate_payment(request):
+    data = request.data
+
+    headers = {
+        "Authorization": f"Bearer {settings.FLW_SECRET_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "tx_ref": data.get("tx_ref"),
+        "amount": data.get("amount"),
+        "currency": "KES",
+        "redirect_url": "http://localhost:3000/payment-success",  # adjust to your React frontend
+        "payment_options": "card,mpesa",
+        "customer": {
+            "email": data.get("email"),
+            "phonenumber": data.get("phonenumber"),
+            "name": data.get("name")
+        },
+        "customizations": {
+            "title": "Interior Health Payment",
+            "description": "Purchase of health product",
+            "logo": "https://your-logo-url.com/logo.png"
+        }
+    }
+
+    res = requests.post("https://api.flutterwave.com/v3/payments", headers=headers, json=payload)
+    return Response(res.json())
+
 
 class OrderCreateView(generics.CreateAPIView):
     serializer_class = OrderSerializer
