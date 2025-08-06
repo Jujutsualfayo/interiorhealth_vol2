60baa22a42d813ebe771e79796eb08a88fa2810e
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-08-06 11:03:12 +0000

    init

--- interiorhealth-backend/config/settings.py
+++ interiorhealth-backend/config/settings.py
@@ -8,7 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost,web', cast=Csv)
+
 
 # Installed apps
 INSTALLED_APPS = [
